91575e306e20c7552e041a5fa4b3e24068b7afb0
Inur123 <zainurroziqin38@gmail.com>
2025-02-12 21:26:26 +0700

    Remove Tailwind CSS integration and update app layout to use alternative script

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <link rel="shortcut icon" type="image/png" href="{{ asset('images/peta-logo.png') }}" />
 
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
     <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/peta-logo.png') }}">
     <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/peta-logo.png') }}">
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/peta-logo.png') }}">
